37eab24d10d1c3aebc84cf9ecd22c1a6716e8b00
pagoulid <pagoulid@gmail.com>
2022-08-31 12:56:56 +0300

    Implement functionality for greater number

---
+++ Greater_Number.js
@@ -0,0 +1,36 @@
+
+function greaterNum(){
+    let fNumVal=document.getElementById('firstNum').value;
+    let sNumVal=document.getElementById('secNum').value;
+
+    compare(fNumVal,sNumVal);
+    
+
+
+}
+
+function compare(num1,num2){
+    let condition =num1>num2?1:0;
+    let compareResult;
+
+    switch(condition){
+        case 0:
+            compareResult=`${num2} is greater than ${num1}`;
+            console.log(`${num2} is greater than ${num1}`);
+            break;
+        case 1:
+            compareResult=`${num1} is greater than ${num2}`;
+            console.log(`${num1} is greater than ${num2}`);
+            break;
+
+        
+    }
+
+    appendResults(compareResult);
+}
+
+function appendResults(result){
+    let pNode = document.getElementById("greater_number_result");
+    pNode.textContent=result;
+
+}
