9014a9c218d8e1161ef53849bb364bcc727f8765
pagoulid <pagoulid@gmail.com>
2022-08-31 17:44:37 +0300

    Completed not!!!Fighting with a bug... :(

--- scripts/Greater_Number.js
+++ scripts/Greater_Number.js
@@ -1,12 +1,13 @@
 import {appendResults,switchCase} from 'https://pagoulid.github.io/JS-Conditional_Statements-and-Loops/scripts/modules/modules.js';
 document.getElementById('GButton').addEventListener('click',greaterNum);
 function greaterNum(){
-    let fNumVal=document.getElementById('firstNum').value;
-    let sNumVal=document.getElementById('secNum').value;
+    let fNumVal=parseInt(document.getElementById('firstNum').value);
+    let sNumVal=parseInt(document.getElementById('secNum').value);
     let condition = fNumVal>sNumVal?1:0;
     let result=switchCase(condition,`${sNumVal} is greater than ${fNumVal}`,`${fNumVal} is greater than ${sNumVal}`);
     appendResults(result,"greater_number_result");
 }
+
 
 
 
