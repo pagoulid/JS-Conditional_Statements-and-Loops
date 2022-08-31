00c1c3171c41a961fe06a510122315193f2388cc
pagoulid <pagoulid@gmail.com>
2022-08-31 15:29:48 +0300

    Add some styling

--- Greater_Number.js
+++ Greater_Number.js
@@ -16,11 +16,9 @@ function compare(num1,num2){
     switch(condition){
         case 0:
             compareResult=`${num2} is greater than ${num1}`;
-            console.log(`${num2} is greater than ${num1}`);
             break;
         case 1:
             compareResult=`${num1} is greater than ${num2}`;
-            console.log(`${num1} is greater than ${num2}`);
             break;
 
         
